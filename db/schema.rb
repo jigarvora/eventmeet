88c84cfb52a47c3cf2dd861c5ee09bff765615db
Ishaan Sutaria <ishaansutaria@Ishaans-MacBook-Pro-2.local>
2015-06-27 15:22:06 -0700

    Adding a join for user and events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627215248) do
+ActiveRecord::Schema.define(version: 20150627221857) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20150627215248) do
     t.date     "date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "events_users", force: :cascade do |t|
+    t.integer "event_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
