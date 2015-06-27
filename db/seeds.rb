861ab94292d9c17a24cab0e6e86cfeabee364570
Jigar Vora <jvora08@gmail.com>
2015-06-27 15:38:13 -0700

    events - make the event for page look good

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+ActiveRecord::Base.establish_connection
+ActiveRecord::Base.connection.tables.each do |table|
+  # SQLite
+  ActiveRecord::Base.connection.execute("DELETE FROM #{table}") unless table == "schema_migrations"
+end
+
+Event.delete_all
+launch = Event.create(title: "Jigar & Ishaan App Launch Party",
+  description: 
+    %{<p>
+        This party is to celebrate the launch of a billion dollar app created by Jigar & Ishaan.
+      </p>},
+  loc_city:   'Sunnyvale',
+  visibility: 0,
+  date: DateTime.new(2015, 7, 7))
+
+User.delete_all
+launch.users.create(name: "Jigar Vora",
+  email: 'jigar@gmail.com',
+  birthday: DateTime.new(2000, 01, 01),
+  gender: 0)
+
+launch.users.create(name: "Ishaan Sutaria",
+  email: 'ishaan@gmail.com',
+  birthday: DateTime.new(2001, 01, 01),
+  gender: 0)
